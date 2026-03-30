96f35777242689645df369ba5df4a951500d3f9f
Koshoff <ivo.koshov@gmail.com>
2026-03-30 22:07:22 +0300

    message

--- backend/src/main/java/com/sociolog/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/sociolog/backend/config/SecurityConfig.java
@@ -117,7 +117,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "https://sociolog.online", 
+                "https://sociolog.online",
                 "https://www.sociolog.online",
                 "https://sociolog.bg",
                 "https://www.sociolog.bg"));
